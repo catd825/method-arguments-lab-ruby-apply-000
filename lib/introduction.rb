2da99c9ab04e0e1a69dd29c91b48662898ac4f43
Cathy D'Onofrio <catd825@gmail.com>
2020-03-14 19:11:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,3 +3,10 @@ name = "Josh"
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
+
+
+name = "Dan"
+language = "Ember.js"
+def introduction(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
